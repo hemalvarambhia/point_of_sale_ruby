0cd367d173e98b9d847edfa68cf263f395395665
Hemal <h.n.varambhia@gmail.com>
2015-05-07 17:15:16 +0100

    Create the catalogue in the constructor, method name made intent-revealing

--- lib/point_of_sale.rb
+++ lib/point_of_sale.rb
@@ -1,23 +1,22 @@
 class PointOfSale
   def initialize display
     @display = display
+    @catalogue = {
+      "123456" => "£1.50",
+      "246810" => "£5.00"
+    }
   end
 
   def on_barcode barcode
     if barcode.empty?
       @display.set_text "Barcode is invalid"
     else
-      @display.set_text find_in_catalogue(barcode)
+      @display.set_text price_from_catalogue(barcode)
     end
   end
 
   private
-  def find_in_catalogue(barcode)
-    catalogue = {
-    	"123456" => "£1.50",
-        "246810" => "£5.00"
-    }
-
-    catalogue.fetch(barcode, "Product not found")
+  def price_from_catalogue(barcode)
+    @catalogue.fetch(barcode, "Product not found")
   end
 end
