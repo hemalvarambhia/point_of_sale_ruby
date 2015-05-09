bc93300666bb24fd110dcb28144b6e6def68b1e9
Hemal Varambhia <h.n.varambhia@gmail.com>
2015-05-09 21:36:08 +0100

    Removed feature envy of display

--- lib/display.rb
+++ lib/display.rb
@@ -3,7 +3,16 @@ class Display
   def initialize
     @text = ""
   end
-  def set_text text
+
+  def print price
+    @text = price
+  end
+
+  def product_not_found
+    @text = "Product not found"
+  end
+  
+  def error text
     @text = text
   end
 end
--- lib/point_of_sale.rb
+++ lib/point_of_sale.rb
@@ -8,27 +8,19 @@ class PointOfSale
 
   def on_barcode barcode
     if barcode == nil || barcode.empty?
-      @display.set_text "Barcode is invalid"
+      @display.error "Barcode is invalid"
       return
     end
 
     if @catalogue.contains?(barcode)
-      display price_from_catalogue(barcode)
+      @display.print price_from_catalogue(barcode)
     else
-      display_product_not_found
+      @display.product_not_found
     end
   end
 
   private
   def price_from_catalogue(barcode)
     @catalogue.price_from_barcode(barcode)
-  end
-
-  def display price
-    @display.set_text price
-  end
-
-  def display_product_not_found
-    @display.set_text "Product not found"
   end
 end
