0d3698e4658c79fe31c69d3dd96489e6e4ba02a5
Hemal <h.n.varambhia@gmail.com>
2015-05-21 13:10:55 +0100

    We can format to 2 d.p.

--- spec/formatting_monetary_amounts_spec.rb
+++ spec/formatting_monetary_amounts_spec.rb
@@ -1,10 +1,11 @@
 describe "Formatting monetary amounts" do
   def format(price_in_pence)
-    "£7.89"
+     price = "%.2f" % (price_in_pence.to_f/100.0)
+    "£#{price}"
   end
 
   context "simplest amount" do
-    [[789, "£7.89"]].each do |price_in_pence, expected_formatted_price|
+    [[789, "£7.89"], [520, "£5.20"]].each do |price_in_pence, expected_formatted_price|
       it "formats #{price_in_pence} as #{expected_formatted_price}" do
         expect(format(price_in_pence)).to eq(expected_formatted_price)
       end
