c6722b195fcae2012337509309f7f23ac67ddf04
Hemal <h.n.varambhia@gmail.com>
2015-05-21 13:15:55 +0100

    We can format prices with leading zeros (< £1.00)

--- spec/formatting_monetary_amounts_spec.rb
+++ spec/formatting_monetary_amounts_spec.rb
@@ -5,7 +5,14 @@ describe "Formatting monetary amounts" do
   end
 
   context "simplest amount" do
-    [[789, "£7.89"], [520, "£5.20"]].each do |price_in_pence, expected_formatted_price|
+    [
+       [789, "£7.89"], 
+       [520, "£5.20"], 
+       [400, "£4.00"], 
+       [0, "£0.00"], 
+       [2, "£0.02"],
+       [37, "£0.37"]
+    ].each do |price_in_pence, expected_formatted_price|
       it "formats #{price_in_pence} as #{expected_formatted_price}" do
         expect(format(price_in_pence)).to eq(expected_formatted_price)
       end
