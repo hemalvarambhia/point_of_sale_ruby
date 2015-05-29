54293e16c96851c4b65bdd7167baf61a8cbc52e2
Hemal <h.n.varambhia@gmail.com>
2015-05-29 09:22:30 +0100

    We format monetary amounts in the conventional way

--- spec/formatting_monetary_amounts_spec.rb
+++ spec/formatting_monetary_amounts_spec.rb
@@ -1,7 +1,11 @@
 describe "Formatting monetary amounts" do
   def format(price_in_pence)
-     price = "%.2f" % (price_in_pence.to_f/100.0)
-    "£#{price}"
+    price = "%.2f" % (price_in_pence.to_f/100.0)
+    pounds, pence = price.split('.')
+    pounds = pounds.reverse.scan(/\d{1,3}/).join(",").reverse
+    price = "£#{pounds}.#{pence}"
+     
+    price
   end
 
   context "simplest amount" do
@@ -11,7 +15,9 @@ describe "Formatting monetary amounts" do
        [400, "£4.00"], 
        [0, "£0.00"], 
        [2, "£0.02"],
-       [37, "£0.37"]
+       [37, "£0.37"],
+       [418976, "£4,189.76"],
+       [210832281, "£2,108,322.81"]
     ].each do |price_in_pence, expected_formatted_price|
       it "formats #{price_in_pence} as #{expected_formatted_price}" do
         expect(format(price_in_pence)).to eq(expected_formatted_price)
