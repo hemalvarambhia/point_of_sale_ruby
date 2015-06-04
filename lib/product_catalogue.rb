389286679afe88bbfb1af52cae2fc62c2c30ac73
Hemal Varambhia <h.n.varambhia@gmail.com>
2015-06-04 22:38:03 +0100

    Introduce the format method to the catalogue

--- lib/product_catalogue.rb
+++ lib/product_catalogue.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class ProductCatalogue
   def initialize(prices_as_txt_by_barcode, prices_in_pence_by_barcode)
     @prices_as_txt_by_barcode = prices_as_txt_by_barcode
@@ -8,7 +9,16 @@ class ProductCatalogue
     @prices_as_txt_by_barcode.has_key?(barcode)
   end
 
+   def format(price_in_pence)
+    price = "%.2f" % (price_in_pence.to_f/100.0)
+    pounds, pence = price.split('.')
+    pounds = pounds.reverse.scan(/\d{1,3}/).join(",").reverse
+    price = "£#{pounds}.#{pence}"
+     
+    price
+  end
+
   def find_then_format_price_from_barcode(barcode)
-    @prices_as_txt_by_barcode[barcode]
+    format @prices_in_pence_by_barcode[barcode]
   end
 end
