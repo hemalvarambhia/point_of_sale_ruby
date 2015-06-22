03e0a2d4f0817938c2ab28fe1a1cb5c5cbf61469
Hemal <h.n.varambhia@gmail.com>
2015-06-22 12:19:03 +0100

    Test for product found removed unnecessary constraints

--- spec/find_price_in_memory_catalog_spec.rb
+++ spec/find_price_in_memory_catalog_spec.rb
@@ -3,7 +3,12 @@ require 'in_memory_catalogue'
 require 'shared_examples_for_catalogue'
 describe InMemoryCatalogue do
   def catalogue_with(barcode, price)
-    InMemoryCatalogue.new({barcode => price})
+    InMemoryCatalogue.new({
+       "not #{barcode}" => Price.pence(50), 
+       barcode => price,
+       "again not #{barcode}" => Price.pence(2000)
+      }
+    )
   end
 
   def catalogue_without(barcode)
