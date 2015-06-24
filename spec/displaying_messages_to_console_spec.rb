8c972d893eb197f400d1dafbe5b787cc9ebc982d
Hemal <h.n.varambhia@gmail.com>
2015-06-24 12:44:43 +0100

    A product not found message can be displayed

---
+++ spec/displaying_messages_to_console_spec.rb
@@ -0,0 +1,30 @@
+describe "Displaying messages to the console" do
+  class ConsoleDisplay
+    def product_not_found_message barcode
+      p "Product not found for #{barcode}"
+    end
+  end
+
+  before :each do
+    @production_output_console = StringIO.new
+  end
+
+  after :each do
+    @production_output_console.truncate 0
+  end
+
+  context "when the product is not found" do
+    it "tells the user there is no product for a barcode" do
+      canvas = StringIO.new
+      $stdout = canvas
+
+      ConsoleDisplay.new.product_not_found_message("654321")       
+     
+      expect(text_from(canvas)).to eq("Product not found for 654321")
+    end    
+  end
+
+  def text_from canvas
+    canvas.string.strip.gsub(/\"/, "")
+  end
+end
