0c68a279fd20306fcda0d4d645c44a74e4dcd1bb
Hemal <h.n.varambhia@gmail.com>
2015-06-24 13:17:08 +0100

    Removed context layers and tested display of multiple messages

--- spec/displaying_messages_to_console_spec.rb
+++ spec/displaying_messages_to_console_spec.rb
@@ -19,24 +19,32 @@ describe "Displaying messages to the console" do
     $stdout = STDOUT
   end
 
-  context "when the product is not found" do
-    it "tells the user there is no product for a barcode" do
-      ConsoleDisplay.new.product_not_found_message("654321")       
+  it "tells the user there is no product for a barcode" do
+    ConsoleDisplay.new.product_not_found_message("654321")       
      
-      expect(text_from(@canvas)).to eq("Product not found for 654321")
-    end    
+    expect(text_from(@canvas)).to eq(["Product not found for 654321"])
   end
 
-  context "Empty barcode" do
-    it "tells the user the bacode was empty" do
+  it "tells the user the bacode was empty" do 
+    ConsoleDisplay.new.empty_barcode_message    
   
-      ConsoleDisplay.new.empty_barcode_message    
-  
-      expect(text_from(@canvas)).to eq("Scanning error: empty barcode")
-    end
+    expect(text_from(@canvas)).to eq(["Scanning error: empty barcode"])
+  end
+
+  it "displays multiple messages" do
+    console_display = ConsoleDisplay.new
+
+    console_display.empty_barcode_message
+    console_display.product_not_found_message "235711"
+    console_display.empty_barcode_message
+
+    expect(text_from(@canvas)).to eq([
+        "Scanning error: empty barcode", 
+        "Product not found for 235711",
+        "Scanning error: empty barcode"])
   end
 
   def text_from canvas
-    canvas.string.strip.gsub(/\"/, "")
+    canvas.string.strip.gsub(/\"/, "").split(/\n/)
   end
 end
