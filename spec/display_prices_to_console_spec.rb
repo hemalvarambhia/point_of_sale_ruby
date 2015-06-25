9fbe30238b69d912423ba2f7d471b292d55cbe3e
Hemal <h.n.varambhia@gmail.com>
2015-06-25 12:18:53 +0100

    We not format a price object rather than an value in cents

--- spec/display_prices_to_console_spec.rb
+++ spec/display_prices_to_console_spec.rb
@@ -1,11 +1,16 @@
+require 'price'
 describe "Formatting monetary amounts" do
-  def format(price_in_pence)
-    price = "%.2f" % (price_in_pence.to_f/100.0)
+  def format(price)
+    price = "%.2f" % (price.in_pounds)
     pounds, pence = price.split('.')
     pounds = pounds.reverse.scan(/\d{1,3}/).join(",").reverse
     price = "£#{pounds}.#{pence}"
      
     price
+  end
+
+  def price_in_pounds(pence)
+    pence.to_f/100.0
   end
  
   context "simplest amount" do
@@ -20,7 +25,7 @@ describe "Formatting monetary amounts" do
        [210832281, "£2,108,322.81"]
     ].each do |price_in_pence, expected_formatted_price|
       it "formats #{price_in_pence} as #{expected_formatted_price}" do
-        expect(format(price_in_pence)).to eq(expected_formatted_price)
+        expect(format(Price.pence(price_in_pence))).to eq(expected_formatted_price)
       end
     end
   end
--- spec/price.rb
+++ spec/price.rb
@@ -1,5 +1,17 @@
 class Price
-  def self.pence amount
-    Price.new
+  def initialize(pence)
+   @pence = pence
+  end
+
+  def self.pence pence
+    Price.new pence
+  end
+
+  def self.in_pounds pence
+    pence.to_f/100
+  end
+
+  def in_pounds
+    @pence.to_f / 100
   end
 end
