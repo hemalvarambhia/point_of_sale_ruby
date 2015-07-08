32338e51db9b7edcd48737e8adcefa55ea02b741
Hemal <h.n.varambhia@gmail.com>
2015-07-08 09:22:58 +0100

    Created format lambda functions

--- spec/console_display.rb
+++ spec/console_display.rb
@@ -1,15 +1,32 @@
 # coding: utf-8
 class ConsoleDisplay
+  PRICE_FORMAT = lambda { |price|
+    price = "%.2f" % (price.in_pounds)
+    pounds, pence = price.split('.')
+    pounds = pounds.reverse.scan(/\d{1,3}/).join(",").reverse
+    price = "£#{pounds}.#{pence}"
+
+    price
+  }
+
+  PRODUCT_NOT_FOUND_FORMAT = lambda { |barcode|
+    "Product not found for %s" % barcode
+  }
+
+  EMPTY_BARCODE_FORMAT = lambda {
+    "Scanning error: empty barcode"
+  } 
+
   def display_price price
-    p format price
+    p PRICE_FORMAT.call(price)
   end
   
   def display_product_not_found_message barcode
-    p "Product not found for %s" % barcode
+    p PRODUCT_NOT_FOUND_FORMAT.call barcode
   end
 
   def display_empty_barcode_message
-    p "Scanning error: empty barcode"
+    p EMPTY_BARCODE_FORMAT.call
   end
 
   private
