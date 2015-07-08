73919d9a9d01bff0eb121f54b8e19c98ba090939
Hemal <h.n.varambhia@gmail.com>
2015-07-08 10:53:14 +0100

    A point of sale terminal that integrates the in memory catalogue and console display

--- lib/virtual_point_of_sale_terminal.rb
+++ lib/virtual_point_of_sale_terminal.rb
@@ -1,1 +1,17 @@
-sale_controller = SaleController.new(InMemoryCatalogue.new, EnglishLanguageConsoleDisplay.new)
+require_relative "./sale_controller"
+require_relative './english_language_console_display'
+require_relative './in_memory_catalogue'
+require_relative './price'
+
+sale_controller = SaleController.new(
+  EnglishLanguageConsoleDisplay.new, 
+  InMemoryCatalogue.new({
+   "12345" => Price.pence(795),
+   "23456" => Price.pence(1250)
+  })
+)
+
+sale_controller.on_barcode "12345"
+sale_controller.on_barcode "23456"
+sale_controller.on_barcode "99999"
+sale_controller.on_barcode ""
