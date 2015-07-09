c67684255d2631379c87af2654d22a30ca218458
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-07-09 08:39:44 +0100

    We can interpret any number of commands so long as they're barcodes

--- spec/interpret_text_commands_spec.rb
+++ spec/interpret_text_commands_spec.rb
@@ -3,12 +3,11 @@ describe "Interpreting text commands" do
     def initialize listener
       @listener = listener
     end
+    
     def process stream
-      line = stream.gets
-      if line
-        barcode = line.strip
-        @listener.on_barcode(barcode)
-      end
+      stream.each_line { |line|
+        @listener.on_barcode(line.strip)
+      }
     end
   end
   
@@ -29,6 +28,18 @@ describe "Interpreting text commands" do
       expect(barcode_listener).to receive(:on_barcode).with("::barcode::").once
 
       TextCommandInterpreter.new(barcode_listener).process StringIO.new("::barcode::\n")
+    end
+  end
+
+  context "several barcodes" do
+    it "process all of them" do
+       barcode_listener = double("BarcodeScannedListener")
+
+       expect(barcode_listener).to receive(:on_barcode).with("12345")
+       expect(barcode_listener).to receive(:on_barcode).with("23456")
+       expect(barcode_listener).to receive(:on_barcode).with("34567")
+
+       TextCommandInterpreter.new(barcode_listener).process StringIO.new("12345\n23456\n34567")
     end
   end
 end
