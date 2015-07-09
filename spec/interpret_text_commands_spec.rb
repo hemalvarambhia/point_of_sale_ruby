8b222829703113a5f1886748eef20e4fc34c7f5a
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-07-09 08:16:33 +0100

    We can interpret at most one command as a barcode

--- spec/interpret_text_commands_spec.rb
+++ spec/interpret_text_commands_spec.rb
@@ -1,15 +1,34 @@
 describe "Interpreting text commands" do
   class TextCommandInterpreter
+    def initialize listener
+      @listener = listener
+    end
     def process stream
+      line = stream.gets
+      if line
+        barcode = line.strip
+        @listener.on_barcode(barcode)
+      end
     end
   end
+  
   context "no commands" do
     it "does nothing" do
       barcode_listener = double("BarcodeScannedListener")
 
-      expect(barcode_listener).to_not receive(:barcode_scanned)
+      expect(barcode_listener).to_not receive(:on_barcode)
 
-      TextCommandInterpreter.new.process StringIO.new("")
+      TextCommandInterpreter.new(barcode_listener).process StringIO.new("")
+    end
+  end
+
+  context "one barcode" do
+    it "processes the barcode" do
+      barcode_listener = double("BarcodeScannedListener")
+
+      expect(barcode_listener).to receive(:on_barcode).with("::barcode::").once
+
+      TextCommandInterpreter.new(barcode_listener).process StringIO.new("::barcode::\n")
     end
   end
 end
